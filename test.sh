80d30bea0038e7579ff9bd4578e5de37d8357fb2
Philippe <philippe_piatti@orange.fr>
2018-12-07 17:25:45 +0100

    Script de rappel du cours sur les commandes bash

---
+++ test.sh
@@ -0,0 +1,11 @@
+echo ""
+echo "bonjour michael"
+echo ""
+echo "je suis ici : "
+pwd
+echo ""
+touch index.html style.css app.js && mkdir style script
+mv style.css style/ 
+mv app.js script/
+echo "c'est fini !"
+echo ""
