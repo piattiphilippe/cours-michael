079b462530458bfa707195dd09e0edeefc0514ea
Philippe <philippe_piatti@orange.fr>
2018-12-28 17:52:42 +0100

    ajout d'une condition

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const age = 50;
+const age = 14.9;
 
 if (age >= 50) {
     console.log("foutu");
@@ -6,6 +6,9 @@ if (age >= 50) {
 else if (age >= 18) {
     console.log("majeur");
 }
+else if (age >= 15) {
+    console.log("mineur")
+}
 else {
-    console.log("mineur");
+    console.log("enfant");
 }
