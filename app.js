9140436d45b553d3530866e64fbffbc4c3544b7b
Philippe <philippe_piatti@orange.fr>
2019-01-11 15:46:48 +0100

    création dynamique d'un menu

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
+const ul = document.querySelector("ul");
+const menu = ["coco", "jaco"];
 
-const titleTag = document.querySelector('h1');
-const colors = ['green', 'blue', 'purple', 'red', 'gray'];
-let compteur = 0;
-
-function addTitle() {
-    let newTitle = prompt("Ajouter un titre ?");
-
-    if (compteur == 5) {
-        compteur = 0;
-    }
-
-    titleTag.innerHTML = `<span style='color:${colors[compteur]}'>${newTitle}</span>`;
-
-    // console.log(colors)
-    // console.log('compteur =', compteur, 'DONC colors['+compteur+'] =', colors[compteur])
-    // console.log('\n')
-    compteur++;
+for (let i = 0; i < menu.length; i++) {
+    ul.innerHTML += "<li>" + menu[i] + "</li>";
 }
+
+ function ajoutMenu() {
+     let onglet = prompt("Crée un onglet");
+     menu.push(onglet);
+     console.log(menu);
+ }
