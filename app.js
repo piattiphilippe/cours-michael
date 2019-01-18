a3cf2dac15a1ba0dfee9981fb74158b05a9262ea
Philippe <philippe_piatti@orange.fr>
2019-01-18 17:29:48 +0100

    Parcours de maps depuis un fichier json

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-const contacts = [
-    ["Philippe", "Piatti", "philippe@gmail.com"],
-    ["Michael", "Cornillon", "michael@gmail.com"],
-    ["Daphné", "Cornilon", "daphne@gmail.com"],
-    ["René", "Hersent", "rené@gmail.com"],
-];
-
-const corpsDuTableauHTML = document.querySelector('tbody');
-let index = 0;
-
-window.onkeyup = function() {
-    if (index >= contacts.length) {
-        return; // Ne retourne rien !!! (stop net la fonction)
+fetch('maps.json').then(data => data.json()).then(function(maps) {
+    for (let map of maps) {
+        const maison = document.querySelector('.maison');
+        let myHtml = '<table>';
+        
+        for (let i = 0; i < map.length; i++) {
+            myHtml += '<tr>';
+            for (let j = 0; j < map[i].length; j++) {
+                switch (map[i][j]) {
+                    case 0:
+                        myHtml += '<td class="ciel"></td>';
+                        break;
+                    case 1:
+                        myHtml += '<td class="terre"></td>';
+                        break;
+                    case 2:
+                        myHtml += '<td class="mur"></td>';
+                        break;
+                    case 3:
+                        myHtml += '<td class="porte"></td>';
+                        break;
+                    case 4:
+                        myHtml += '<td class="toit"></td>';
+                        break;
+                    case 5:
+                        myHtml += '<td class="nuage"></td>';
+                        break;
+                    case 6:
+                        myHtml += '<td class="tronc"></td>';
+                        break;
+                    case 7:
+                        myHtml += '<td class="cime"></td>';
+                        break;
+                }
+            }
+            myHtml += '</tr>';
+        }
+        
+        myHtml += '</table>';
+        
+        maison.innerHTML += myHtml;
     }
-
-    corpsDuTableauHTML.innerHTML += `
-        <tr>
-            <td>${contacts[index][0]}</td>
-            <td>${contacts[index][1]}</td>
-            <td>${contacts[index][2]}</td>
-        </tr>
-    `;
-    index++;
-};
+})
