8db5b1c6700662bc094265f2cc23b2a9a8c1a9c7
Philippe <philippe_piatti@orange.fr>
2019-01-18 14:20:53 +0100

    Amélioration du menu

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
 const ul = document.querySelector("ul");
-const menu = ["coco", "jaco"];
+const menu = ["<i class='fas fa-home'> Accueil</i>"];
+let user = 'member';
 
-for (let i = 0; i < menu.length; i++) {
-    ul.innerHTML += "<li>" + menu[i] + "</li>";
+function ajoutMenu() {
+    let onglet = prompt("Crée un onglet");
+
+    if (onglet !== null && onglet !== "") {
+        menu.push(onglet);
+        afficherMenu(true);
+    }
+
+    // console.log(menu);
 }
 
- function ajoutMenu() {
-     let onglet = prompt("Crée un onglet");
-     menu.push(onglet);
-     console.log(menu);
- }
+function afficherMenu(ajouterElement) {
+    if (ajouterElement) {
+        ul.innerHTML += construitUnItemDeMenu(menu.length-1);
+    }
+    else { // Enlever un élément du menu
+        for (let i = 0; i < menu.length; i++) {
+            if (i == 0) ul.innerHTML = construitUnItemDeMenu(i);
+            else ul.innerHTML += construitUnItemDeMenu(i);
+        }
+    }
+}
+
+function construitUnItemDeMenu(index) {
+    return "<li class='nav-item'><a class='nav-link' href='#'>" + menu[index] + "</a></li>";
+}
+
+function supprimerMenu() {
+    menu.pop();
+    afficherMenu();
+}
+
+function construitLesBoutonsDAdministration() {
+    document.body.innerHTML += `
+        <button onclick="ajoutMenu()"><i class="fas fa-plus"></i></button>
+        <button onclick="supprimerMenu()"><i class="far fa-trash-alt"></i></button>
+    `;
+}
+
+afficherMenu(true);
+
+if (user === 'admin') {
+    construitLesBoutonsDAdministration();
+}
