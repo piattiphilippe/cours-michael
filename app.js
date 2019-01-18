2f97f4bff52434140484fc8498211f56b9eb3f68
Philippe <philippe_piatti@orange.fr>
2019-01-18 15:29:28 +0100

    Parcours d'un tableau avec for of

--- app.js
+++ app.js
@@ -1,48 +1,12 @@
-const ul = document.querySelector("ul");
-const menu = ["<i class='fas fa-home'> Accueil</i>"];
-let user = 'member';
+const ages = [15, 24, 3, 58, 42, 17, 85, 2];
 
-function ajoutMenu() {
-    let onglet = prompt("Crée un onglet");
-
-    if (onglet !== null && onglet !== "") {
-        menu.push(onglet);
-        afficherMenu(true);
-    }
-
-    // console.log(menu);
+// Parcours du tableau ages
+for (let i = 0; i < ages.length; i++) {
+    console.log("tu a " + ages[i] + " ans");
 }
 
-function afficherMenu(ajouterElement) {
-    if (ajouterElement) {
-        ul.innerHTML += construitUnItemDeMenu(menu.length-1);
-    }
-    else { // Enlever un élément du menu
-        for (let i = 0; i < menu.length; i++) {
-            if (i == 0) ul.innerHTML = construitUnItemDeMenu(i);
-            else ul.innerHTML += construitUnItemDeMenu(i);
-        }
-    }
-}
 
-function construitUnItemDeMenu(index) {
-    return "<li class='nav-item'><a class='nav-link' href='#'>" + menu[index] + "</a></li>";
-}
-
-function supprimerMenu() {
-    menu.pop();
-    afficherMenu();
-}
-
-function construitLesBoutonsDAdministration() {
-    document.body.innerHTML += `
-        <button onclick="ajoutMenu()"><i class="fas fa-plus"></i></button>
-        <button onclick="supprimerMenu()"><i class="far fa-trash-alt"></i></button>
-    `;
-}
-
-afficherMenu(true);
-
-if (user === 'admin') {
-    construitLesBoutonsDAdministration();
+// La même chose encore plus rapide
+for (let age of ages) {
+    console.log(`tu a ${age} ans`);
 }
